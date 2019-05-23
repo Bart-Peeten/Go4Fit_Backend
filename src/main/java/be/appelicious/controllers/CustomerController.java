3a4ac56b96665bcdb875fe7f8073d9e45d48c0f1
Bart-Peeten <bart.peeten@gmail.com>
2019-05-23 22:53:09 +0200

    Add method to check if user exsist

--- src/main/java/be/appelicious/controllers/CustomerController.java
+++ src/main/java/be/appelicious/controllers/CustomerController.java
@@ -1,7 +1,16 @@
 package be.appelicious.controllers;
 
+import be.appelicious.domain.Customer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/users")
 public class CustomerController {
+
+    public ResponseEntity doesUserExsist(Customer user){
+        return null;
+    }
+
 }
