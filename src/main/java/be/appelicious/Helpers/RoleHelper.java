1d301bf8dcac386c979c5b7ce1973c307cfa52e1
Bart Peeten <bart.peeten@beeyond.nl>
2019-08-01 13:57:24 +0200

    Add initial security && added new endpoints in the CustomerController

---
+++ src/main/java/be/appelicious/Helpers/RoleHelper.java
@@ -0,0 +1,11 @@
+package be.appelicious.Helpers;
+
+public class RoleHelper {
+
+    /** This class defines the roles for security level
+     * which can be used within the whole project
+     * If new roles are defined add them here*/
+
+    public final static String USER = "ROLE_USER";
+    public final static String ADMIN = "ROLE_ADMIN";
+}
---
+++ src/main/java/be/appelicious/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package be.appelicious;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().
+
+                authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth, DataSource ds) throws Exception {
+        auth.jdbcAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(ds)
+                .usersByUsernameQuery(
+                        "SELECT u.userId, u.password, u.enabled FROM s2it_user u where u.userId = ?")
+                .authoritiesByUsernameQuery(
+                        "SELECT u.userId, u.role from s2it_user u where u.userId = ?");
+    }
+}
